4a1ca4c9d5395ce4a5da09b75de99c646c8d6bda
sulphurninja <sulphurninja@gmail.com>
2024-07-31 23:37:28 +0530

    Policies and terms added

---
+++ src/app/contactus/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+export default function ContactUs({ }: Props) {
+    return (
+        <div>
+            Contact Us
+            Last updated on 31-07-2024 23:33:58
+            You may contact us using the information below:
+            Merchant Legal entity name: BASHIR MEHBOOB SHAIKH
+            Registered Address: MIDC Road, Shinde Vasti, Pune, Maharashtra, PIN: 412101
+            Operational Address: MIDC Road, Shinde Vasti, Pune, Maharashtra, PIN: 412101
+            Telephone No: 7757875760
+            E-Mail ID: pakaak125@gmail.com
+        </div>
+    )
+}
