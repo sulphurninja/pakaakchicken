feabc39079f0e0c6693a9dfe136ca07838350235
sulphurninja <sulphurninja@gmail.com>
2024-08-08 19:27:53 +0530

    Auth Api's Done

--- src/store/GlobalState.js
+++ src/store/GlobalState.js
@@ -1,3 +1,5 @@
+'use client'
+
 import { createContext, useReducer, useEffect } from "react";
 import reducers from "./Reducers";
 import { getData } from "../utils/fetchData";
@@ -9,16 +11,16 @@ export const DataProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducers, initialState)
 
 
-    useEffect(()=>{
+    useEffect(() => {
         const firstLogin = localStorage.getItem('firstLogin');
-        if(firstLogin){
+        if (firstLogin) {
             getData('/auth/accessToken').then(res => {
-                if(res.err) return localStorage.removeItem("firstLogin")
+                if (res.err) return localStorage.removeItem("firstLogin")
 
                 dispatch({
                     type: "AUTH",
-                    payload:{
-                        token:res.access_token,
+                    payload: {
+                        token: res.access_token,
                         user: res.user,
                     }
                 })
